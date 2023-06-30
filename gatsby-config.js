4ae9af5d2a25503bf4cc26f80f25f3e2a45deeb7
jp <jayantpahuja9@gmail.com>
2023-06-30 15:57:23 +0530

    Update OG Images

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,7 @@ module.exports = {
     title: 'Jayant Pahuja',
     description:
       'Jayant Pahuja is a full software engineer who specializes in building exceptional digital experiences.',
-    // TODO: JP TO update this
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    siteUrl: 'https://jayant.fyi', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
   },
   plugins: [
